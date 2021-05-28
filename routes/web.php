0244223490bc38e1239869f8fcca36be6d64aa76
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-05-28 15:06:12 +0200

    Create roles

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,4 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('departments', DepartmentController::class);
+Route::resource('roles', RoleController::class);
