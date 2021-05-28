ea48f1d6405a1fce697401989dd008f91c3a9546
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-05-28 17:49:56 +0200

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,6 +15,8 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::resource('departments', DepartmentController::class);
-Route::resource('roles', RoleController::class);
-Route::resource('users', UserController::class);
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+});
