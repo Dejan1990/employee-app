5d58f464501bc97faad57d994fb357f074db3e8e
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-05-29 16:57:38 +0200

    Create leave form

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
@@ -21,4 +22,5 @@ Route::group(['middleware' => ['auth', 'has.permission']], function () {
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
     Route::resource('permissions', PermissionController::class);
+    Route::resource('leaves', LeaveController::class);
 });
